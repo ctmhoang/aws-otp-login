09fc37a1fd92b0c734b6d1c8431e4b5bb00b1b4e
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-04-09 08:44:40 +0700

    Vertify OTP

--- lib/main.dart
+++ lib/main.dart
@@ -74,16 +74,27 @@ class _AmpliflutterState extends State<Ampliflutter> {
                   decoration: InputDecoration(labelText: "OTP"),
                   controller: _otp,
                 ),
-                RaisedButton(
-                    child: Text("CREATE ACCOUNT"),
-                    onPressed: () => _createAccountOnPressed(context),
-                    color: Theme.of(context).primaryColor,
-                    colorBrightness: Theme.of(context).primaryColorBrightness),
-                RaisedButton(
-                    child: Text("SignIn ACCOUNT"),
-                    onPressed: () => _signIn(context),
-                    color: Theme.of(context).primaryColor,
-                    colorBrightness: Theme.of(context).primaryColorBrightness),
+                ElevatedButton(
+                  child: Text("CREATE ACCOUNT"),
+                  onPressed: () => _createAccountOnPressed(context),
+                  style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      onPrimary: Theme.of(context).primaryColorLight),
+                ),
+                ElevatedButton(
+                  child: Text("SIGNIN ACCOUNT"),
+                  onPressed: () => _signIn(context),
+                  style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      onPrimary: Theme.of(context).primaryColorLight),
+                ),
+                ElevatedButton(
+                  child: Text("VERIFY ACCOUNT"),
+                  onPressed: () => _verify(context),
+                  style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      onPrimary: Theme.of(context).primaryColorLight),
+                ),
               ],
               mainAxisAlignment: MainAxisAlignment.center,
             )));
@@ -98,10 +109,10 @@ class _AmpliflutterState extends State<Ampliflutter> {
     try {
       SignUpResult res = await Amplify.Auth.signUp(
           username: phone,
-          password: '123',
+          password: '111111Abc@',
           options: CognitoSignUpOptions(userAttributes: {
             'phone_number': phone,
-            "email": 'leon.tuanhm@gmail.com'
+            "email": 'leon.mytv@gmail.com'
           }));
       print('lol ${res.isSignUpComplete}');
     } catch (e) {
@@ -116,7 +127,8 @@ class _AmpliflutterState extends State<Ampliflutter> {
     final phone = _phoneController.text;
     SignInResult cognitoUser;
     try {
-      cognitoUser = await Amplify.Auth.signIn(username: phone, password: '123');
+      cognitoUser =
+          await Amplify.Auth.signIn(username: phone, password: '111111Abc@');
       if (cognitoUser.isSignedIn) {
         print('sent');
       }
@@ -130,6 +142,23 @@ class _AmpliflutterState extends State<Ampliflutter> {
         await Amplify.Auth.confirmSignIn(confirmationValue: _otp.text);
     if (tmp.isSignedIn) {
       print('success');
+    }
+  }
+
+  Future<void> _verify(BuildContext context) async {
+    final phone = _phoneController.text;
+    final otp = _otp.text;
+    SignUpResult cognitoUser;
+    try {
+      cognitoUser = await Amplify.Auth.confirmSignUp(
+          username: phone, confirmationCode: '111111');
+      if (cognitoUser.isSignUpComplete) {
+        print('lol signup');
+      }
+    } catch (e) {
+      // Handle sign in errors
+      print('error lol');
+      print(e);
     }
   }
 }
